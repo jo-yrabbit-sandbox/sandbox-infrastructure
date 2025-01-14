c5d8197958acbd0f6d534db1ec763db7815db007
Jolly Rabbit <jollyrabbitok@gmail.com>
2025-01-14 08:09:16 -0800

    Add dependency so that terraform cannot delete service-linked role

--- bot-platform/terraform/iam.tf
+++ bot-platform/terraform/iam.tf
@@ -92,4 +92,9 @@ resource "aws_iam_role_policy" "bot_task_policy" {
 resource "aws_iam_service_linked_role" "ecs" {
   aws_service_name = "ecs.amazonaws.com"
   description      = "Role to enable Amazon ECS to manage your cluster."
+
+  # Prevent Terraform from trying to delete this role
+  lifecycle {
+    prevent_destroy = true
+  }
 }
